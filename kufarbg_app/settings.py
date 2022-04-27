5931170ca4cbc6db24054e00c4a39b269c6efc00
Ivan <ivan.yosifov88@gmail.com>
2022-04-28 00:12:47 +0300

    Added changes to DEBUG config

--- kufarbg_app/settings.py
+++ kufarbg_app/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'debug_key')
 
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
     os.getenv('APP_HOST', '127.0.0.1')
