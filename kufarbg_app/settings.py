0c729e00b7484d9bd0e9e6159f9b507ae56e4e74
Ivan <ivan.yosifov88@gmail.com>
2022-04-27 17:24:55 +0300

    Added requrements.txt

--- kufarbg_app/settings.py
+++ kufarbg_app/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # TODO UNMARK PASSWORD
@@ -101,11 +102,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
